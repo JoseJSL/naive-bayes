806427f66c1bec9761d22e1a2232c55c5e638803
Krusper <krusper28@gmail.com>
2021-12-08 20:31:45 -0700

    Calculo de matriz de confusión, TP, TN, FP, FN, precisión, recall, medida f1, accuracy . Agregado una función para obtener el índice de una clase

--- Form1.vb
+++ Form1.vb
@@ -3,6 +3,9 @@
     Dim externalDataset As New List(Of String())
     Dim columnCount As Integer = 0
     Dim frecuencias() As Int32
+    Dim predict As New List(Of String)
+    Dim mtz_conf As New List(Of Integer())
+    Dim clases As New List(Of String)
 
     'Regresa un arreglo tipo List(Of String), según un índice de un dataset, Ej:
     'Dim columna As List(Of String) = getDatasetColumn(mainDataset, 3)
@@ -83,7 +86,7 @@
 
 
         n_intervalos = CInt(txtIntervals.Value)
-        
+
 
         For Each num In arreglo
             arreglo_numero.add(CDbl(num))
@@ -116,4 +119,133 @@
         Return arreglo_nuevo
 
     End Function
+
+    Private Sub calc_mtz_confu(ByRef original As List(Of String))
+
+        Dim ico, ifi As New Integer
+
+        For i = 0 To predict.Count - 1
+            If predict.ElementAt(i) = original.ElementAt(i) Then
+                ico = class_indi(predict.ElementAt(i))
+                mtzval_up(ico, ico)
+
+            Else
+                ico = class_indi(predict.ElementAt(i))
+                ifi = class_indi(original.ElementAt(i))
+                mtzval_up(ico, ifi)
+
+            End If
+
+        Next
+
+    End Sub
+
+
+    Private Function class_indi(ByRef clase As String) As Integer
+
+        For i = 0 To clases.Count - 1
+            If clases.ElementAt(i) = clase Then
+                Return i
+
+            End If
+        Next
+
+        Return -1
+    End Function
+
+    Private Sub mtzval_up(ByRef inc As Integer, ByRef inf As Integer)
+        Dim v As Integer = mtz_conf.ElementAt(inc).ElementAt(inf)
+
+        mtz_conf.ElementAt(inc).SetValue(v + 1, inf)
+
+    End Sub
+
+    Private Function tp(ByRef ind As Integer) As Integer
+
+        Return mtz_conf.ElementAt(ind).ElementAt(ind)
+    End Function
+
+    Private Function tn(ByRef ind As Integer) As Integer
+        Dim trun As Integer = 0
+
+        For Each columna In mtz_conf
+            For Each fila In columna
+                trun += fila
+            Next
+        Next
+
+        trun -= (tp(ind) + fp(ind) + fn(ind))
+
+        Return trun
+    End Function
+
+    Private Function fp(ByRef inf As Integer) As Integer
+        Dim falp As Integer = 0
+
+        For inc = 0 To clases.Count - 1
+            falp += mtz_conf.ElementAt(inf).ElementAt(inc)
+        Next
+        falp -= tp(inf)
+
+        Return falp
+    End Function
+
+    Private Function fn(ByRef inc As Integer) As Integer
+        Dim faln As Integer = 0
+
+        For inf = 0 To clases.Count - 1
+            faln += mtz_conf.ElementAt(inc).ElementAt(inf)
+        Next
+        faln -= tp(inc)
+
+        Return faln
+    End Function
+
+    Private Function prec(ByRef ind As Integer) As Double
+        Dim preci As Double
+        Dim trupo As Integer
+
+        trupo = tp(ind)
+
+        preci = trupo / (trupo - fp(ind))
+
+        Return preci
+    End Function
+
+    Private Function recall(ByRef ind As Integer) As Double
+        Dim rec As Double
+        Dim trupo As Integer
+        trupo = tp(ind)
+
+        rec = trupo / (trupo - fn(ind))
+
+        Return rec
+    End Function
+
+    Private Function accuracy(ByRef indi As Integer) As Double
+        Dim accu As Double
+        Dim totalmtz, tepes As Integer
+
+        totalmtz = predict.Count
+
+        For i = 0 To clases.Count - 1
+            tepes += tp(i)
+        Next
+
+        accu = tepes / totalmtz
+
+        Return accu
+    End Function
+
+    Private Function mf1(ByRef ind As Integer) As Double
+        Dim medf, preci, recal As Double
+        preci = prec(ind)
+        recal = recall(ind)
+
+        medf = 2 * preci * recal / (preci + recal)
+
+        Return medf
+    End Function
+
+
 End Class
